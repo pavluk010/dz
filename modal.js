f7174dbe9d27f5f340e09de3dbeb2b513d24105b
pavluk001 <sashapavluk24@gmail.com>
2026-06-07 15:34:07 +0300

    fix

---
+++ modal.js
@@ -0,0 +1,19 @@
+(() => {
+  const openBtn = document.querySelector(".open-modal-btn");
+  const closeBtn = document.querySelector(".modal-close");
+  const backdrop = document.querySelector(".backdrop");
+
+  openBtn.addEventListener("click", () => {
+    backdrop.classList.add("is-open");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    backdrop.classList.remove("is-open");
+  });
+
+  backdrop.addEventListener("click", (e) => {
+    if (e.target === backdrop) {
+      backdrop.classList.remove("is-open");
+    }
+  });
+})();
